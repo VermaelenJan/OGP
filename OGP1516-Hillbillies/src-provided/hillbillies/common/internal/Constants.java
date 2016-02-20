fdf83b3bd8215ceb6b4768cfd85e3a37b32cc998
Jan Vermaelen <vermaelen_jan@hotmail.com>
2016-02-20 18:54:43 +0100

    fixed test

--- OGP1516-Hillbillies/src-provided/hillbillies/common/internal/Constants.java
+++ OGP1516-Hillbillies/src-provided/hillbillies/common/internal/Constants.java
@@ -9,7 +9,7 @@ public interface Constants {
 
 	public static final double SIDEBAR_WIDTH = MINIMAP_SIZE + 50;
 
-	public static final int CUBE_SIZE = 32;// pixels per cube
+	public static final int CUBE_SIZE = 32; // pixels per cube
 
 	public static final double WORLD_VIEW_WIDTH = CUBE_SIZE * ((int) ((WINDOW_WIDTH - SIDEBAR_WIDTH - 50) / CUBE_SIZE));
 	public static final double WORLD_VIEW_HEIGHT = CUBE_SIZE * ((int) ((WINDOW_HEIGHT - 50) / CUBE_SIZE));
