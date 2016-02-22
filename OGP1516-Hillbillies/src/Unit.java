0c975b373352526ec2bf47fd461f24fc4b70127c
0maxime <pittomvils-maxime@hotmail.com>
2016-02-22 15:59:08 +0100

    set/get agility,hitpoints,stamina,orientation,toughness

--- OGP1516-Hillbillies/src/Unit.java
+++ OGP1516-Hillbillies/src/Unit.java
@@ -15,11 +15,14 @@ public class Unit { //TODO: put (x,y,z) into a list or...
 	private double x_pos;
 	private double y_pos;
 	private double z_pos;
-//	private String name;
+	private String name;
 	private int weight;
 	private int strength;
 	private int agility;
-//	private float toughness;
+	private int toughness;
+	private int hitpoints;
+	private int stamina;
+	private float orientation;
 	private boolean flag;
 	private static int init_min_val = 25;
 	private static int init_max_val = 100;
@@ -27,14 +30,19 @@ public class Unit { //TODO: put (x,y,z) into a list or...
 	private static int max_val = 200;
 	private int curr_min_val;
 	private int curr_max_val;
+	// afronden naar bovenste integer, dus + 1??
+	private int max_hitpoints_stamina = (this.getWeight()*this.getToughness())/50;;
 
-	public Unit(double x, double y, double z, String name, int weight, int strength, int agility, int toughness, boolean flag) throws IllegalPositionException {
+	public Unit(double x, double y, double z, String name, int weight, int strength, int agility, int toughness, int hitpoints, int stamina, float orientation, boolean flag) throws IllegalPositionException {
 		this.setLocation(x,  y,  z);
-//		this.name = name;
+//		setName(name);
 		setWeight(weight);
 		setStrength(strength);
 		setAgility(agility);
-//		this.toughness = toughness;
+		setToughness(toughness);
+		setHitpoints(hitpoints);
+		setStamina(stamina);
+		setOrientation(orientation);
 	}
 	
 	public void setLocation(double x, double y, double z) throws IllegalPositionException{
@@ -93,7 +101,6 @@ public class Unit { //TODO: put (x,y,z) into a list or...
 			this.weight = (this.getStrength()+this.getAgility())/2;
 			
 		}
-// T0D0 add getstrength getagility
 
 	}
 	
@@ -105,10 +112,18 @@ public class Unit { //TODO: put (x,y,z) into a list or...
 		return this.weight;
 	}	
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public int getStrength(){
 		return this.strength;
 	}
 	
+	/**
+	 * 
+	 * @param strength
+	 */
 	public void setStrength(int strength){
 		if (this.flag = false){
 			setFlag(true);
@@ -128,17 +143,146 @@ public class Unit { //TODO: put (x,y,z) into a list or...
 			this.strength = curr_max_val; 
 	}
 	
+	/**
+	 * 
+	 * @param agility
+	 */
+	public void setAgility(int agility){
+		if (this.flag = false){
+			setFlag(true);
+			curr_min_val = init_min_val;
+			curr_max_val = init_max_val;
+		}
+		else {
+			curr_min_val = min_val;
+			curr_max_val = max_val;
+		}
+
+		if ( agility < curr_min_val) 
+			this.agility = curr_min_val;
+		else if ((agility >= curr_min_val) && (agility <= curr_max_val))
+			this.agility = agility;
+		else if (agility > curr_max_val)
+			this.agility = curr_max_val; 
+	}
+	/**
+	 * 
+	 * @return
+	 */
 	public int getAgility(){
 		return this.agility;
 	}
 	
+	/**
+	 * 
+	 * @param toughness
+	 */
+	public void setToughness(int toughness){
+		if (this.flag = false){
+			setFlag(true);
+			curr_min_val = init_min_val;
+			curr_max_val = init_max_val;
+		}
+		else {
+			curr_min_val = min_val;
+			curr_max_val = max_val;
+		}
+
+		if ( toughness < curr_min_val) 
+			this.toughness = curr_min_val;
+		else if ((toughness >= curr_min_val) && (toughness <= curr_max_val))
+			this.toughness =toughness;
+		else if (toughness > curr_max_val)
+			this.toughness = curr_max_val; 
+	}
+	/**
+	 * 
+	 * @return
+	 */
+	public int getToughness(){
+		return this.toughness;
+	}
+	/**
+	 * 
+	 * @param flag
+	 */
 	public void setFlag(boolean flag){
 		this.flag = flag;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
 	public boolean getFlag(){
 		return this.flag;
 	}
+	
+	public boolean isValidHitpoints(int hitpoints){
+		return ((hitpoints >= 0) && (hitpoints <= max_hitpoints_stamina))
+	}
+	
+	public boolean isValidStamina(int stamina){
+		return ((stamina >= 0) && (stamina <= max_hitpoints_stamina))
+	}
+	
+	/**
+	 * 
+	 * @param hitpoints
+	 * 
+	 * @pre
+	 */
+	public void setHitpoints(int hitpoints){
+		assert isValidHitpoints(hitpoints);
+		this.hitpoints = hitpoints;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getHitpoints(){
+		return this.hitpoints;
+	}
+	
+	/**
+	 * 
+	 * @param stamina
+	 * 
+	 * @pre
+	 */
+	public void setStamina(int stamina){
+		assert isValidStamina(stamina);
+		this.stamina = stamina;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public int getStamina(){
+		return this.stamina;
+	}
+	
+	/**
+	 * 
+	 * @param orientation
+	 */
+	public void setOrientation(float orientation){
+		this.orientation = orientation;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public float getOrientation(){
+		return this.orientation;
+	}
+
+	
+	
+	
 	
 	
 	
