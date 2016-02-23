dff00e707da75a184781feab7894c1a6ce84b6aa
Jan Vermaelen <vermaelen_jan@hotmail.com>
2016-02-23 13:27:52 +0100

    added constructor (default orientation)

--- OGP1516-Hillbillies/src/hillbillies/model/Unit.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Unit.java
@@ -25,7 +25,7 @@ public class Unit {
 	private int toughness;
 	private int hitpoints;
 	private int stamina;
-	private float orientation;
+	private double orientation;
 	private static int init_min_val = 25;
 	private static int init_max_val = 100;
 	private static int min_val = 1;
@@ -81,7 +81,7 @@ public class Unit {
 	 * 
 	 */
 	public Unit(List<Double> location, String name, int weight, int strength, int agility, int toughness, 
-			int hitpoints, int stamina, float orientation) throws IllegalPositionException, IllegalNameException {
+			int hitpoints, int stamina, double orientation) throws IllegalPositionException, IllegalNameException {
 		setLocation(location);
 		setName(name);
 		setWeight(weight, true);
@@ -91,6 +91,11 @@ public class Unit {
 		setHitpoints(hitpoints);
 		setStamina(stamina);
 		setOrientation(orientation);
+	}
+	
+	public Unit(List<Double> location, String name, int weight, int strength, int agility, int toughness, 
+			int hitpoints, int stamina) throws IllegalPositionException, IllegalNameException{
+		this(location, name, weight, strength, agility, toughness, hitpoints, stamina, Math.PI/2);
 	}
 	
 	/**
@@ -417,7 +422,7 @@ public class Unit {
 	 * 
 	 * @param orientation
 	 */
-	public void setOrientation(float orientation){
+	public void setOrientation(double orientation){
 		this.orientation = orientation;
 	}
 	
@@ -425,7 +430,7 @@ public class Unit {
 	 * 
 	 * @return
 	 */
-	public float getOrientation(){
+	public double getOrientation(){
 		return this.orientation;
 	}
 	
