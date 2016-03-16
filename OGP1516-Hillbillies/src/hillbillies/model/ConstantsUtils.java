cf3d836986c1d12ad96a9da44f9f52ab13da38ea
Jan Vermaelen <vermaelen_jan@hotmail.com>
2016-03-16 11:32:35 +0100

    constants --> final

--- OGP1516-Hillbillies/src/hillbillies/model/ConstantsUtils.java
+++ OGP1516-Hillbillies/src/hillbillies/model/ConstantsUtils.java
@@ -10,19 +10,19 @@ final class ConstantsUtils {
 	
 
 	// WORLD DIMENSIONS
-	static int WORLD_X = 50;
-	static int WORLD_Y = 50;
-	static int WORLD_Z = 50;
-	static double CUBE_LENGTH = 1;
+	static final int WORLD_X = 50;
+	static final int WORLD_Y = 50;
+	static final int WORLD_Z = 50;
+	static final double CUBE_LENGTH = 1;
 	
 	// MIN,MAX VALUES (for properties)
-	static int INIT_MIN_VAL = 25;
-	static int INIT_MAX_VAL = 100;
-	static int MIN_VAL = 1;
-	static int MAX_VAL = 200;
+	static final int INIT_MIN_VAL = 25;
+	static final int INIT_MAX_VAL = 100;
+	static final int MIN_VAL = 1;
+	static final int MAX_VAL = 200;
 	
 	// Randomizer
-	static Random random = new Random();
+	static final Random random = new Random();
 	
 	// LOG
 	static final int MIN_LOG_WEIGHT = 10;	
