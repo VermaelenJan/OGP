d61e0f30ed1f56f53ec4c314d3b90671ce0d8809
Jan Vermaelen <vermaelen_jan@hotmail.com>
2016-03-23 12:27:03 +0100

    pfff

--- OGP1516-Hillbillies/src/hillbillies/part2/facade/Facade.java
+++ OGP1516-Hillbillies/src/hillbillies/part2/facade/Facade.java
@@ -229,7 +229,7 @@ public class Facade implements IFacade {
 	public World createWorld(int[][][] terrainTypes, TerrainChangeListener modelListener) throws ModelException { //TODO: nakijken!
 		hillbillies.model.CubeType[][][] worldCubes =  //TODO: mag dit in facade? :p
 				new hillbillies.model.CubeType[terrainTypes[0].length][terrainTypes[1].length][terrainTypes[2].length];
-		for (int xIndex = 0; xIndex<terrainTypes.length; xIndex++) { //TODO: DIT OVERAL ZO DOEN!!!
+		for (int xIndex = 0; xIndex<terrainTypes.length; xIndex++) { //TODO: DIT IN WORLD ZELF DOEN!!!! (hopelijk efficienter)
 			for (int yIndex = 0; yIndex<terrainTypes[0].length; yIndex++) {
 				for (int zIndex = 0; zIndex<terrainTypes[0][0].length; zIndex++) {
 					worldCubes[xIndex][yIndex][zIndex] = intToCubeType(terrainTypes[xIndex][yIndex][zIndex]);
