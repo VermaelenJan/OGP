43c07c86dd666550147cbe0a93088521e6cc65dc
0maxime <pittomvils-maxime@hotmail.com>
2016-04-02 00:54:33 +0200

    doc object

--- OGP1516-Hillbillies/src/hillbillies/model/Object.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Object.java
@@ -3,6 +3,8 @@ package hillbillies.model;
 import be.kuleuven.cs.som.annotate.*;
 
 /** 
+ * A class of objects that can occur and used by units in a world.
+ * 
  * @invar  Each object can have its weight as weight.
  *       | canHaveAsWeight(this.getWeight())
  *       
@@ -10,30 +12,51 @@ import be.kuleuven.cs.som.annotate.*;
  * @version 0
  */
 public abstract class Object {
-	//Location
 
-	protected World world;
-	protected Position positionObj;
-
+	/**
+	 * Initialize this new ownable with given owner and given value.
+	 * 
+	 * @param world
+	 * 		The world for this new object.
+	 * @param location
+	 * 		The location for this new object.
+	 * @effect The world of this new object is set to the given world.
+	 * @effect The weight of this new unit is set to a random weight between 10 and 50.
+	 * @effect The location of this new unit is set to the given location.
+	 */
 	protected Object(World world, double[] location){
 		positionObj = new Position(world);
 		positionObj.setFreeLocation(location);
-		this.weight = (ConstantsUtils.random.nextInt(ConstantsUtils.MAX_OBJECT_WEIGHT + 1)+10);
+		this.weight = (ConstantsUtils.random.nextInt(ConstantsUtils.MAX_OBJECT_WEIGHT + 1)-10);
 		this.world = world;
 	}
 	
 	/**
+	 * Variable registering the world of this object.
+	 */
+	protected World world;
+	
+	/**
 	 * Return the location of this object.
 	 */
-	@Basic @Raw
+	@Raw
 	public double[] getLocation() {
 		return positionObj.getLocation();
 	}
 	
-	@Basic @Raw
+	/**
+	 * Return the occupied cube of this object.
+	 */
+	@Raw
 	protected int[] getOccupiedCube(){
 		return positionObj.getOccupiedCube();
 	}
+	
+	/**
+	 * Variable registering the position of this object.
+	 */
+	protected Position positionObj;
+	
 	
 	/**
 	 * Return the weight of this object.
@@ -48,7 +71,7 @@ public abstract class Object {
 	 *  
 	 * @param  weight
 	 *         The weight to check.
-	 * @return The weight will be between 10 and 50.
+	 * @return True if and only if the weight is between 10 and 50.
 	*/
 	@Raw
 	protected boolean canHaveAsWeight(int weight) {
@@ -56,12 +79,22 @@ public abstract class Object {
 				&& (weight <= ConstantsUtils.MAX_OBJECT_WEIGHT));
 	}
 	
-	
 	/**
 	 * Variable registering the weight of this object.
 	 */
 	private final int weight;
 	
+	/**
+	 * Advances the game with duration dt.
+	 * 
+	 * @param dt
+	 * 		The duration which the game time is advanced.
+	 * @effect If the position of the object is not a valid Z position, the unit falls
+	 * 		with its target the cube below.
+	 * @effect If the position of the object is not a valid Z position and the object
+	 * 		is not at the middle of a cube on z level, the units falls to the middle
+	 * 		of the z level of the cube the object is in.  
+	 */
 	protected void advanceTime(double dt){
 		if (!positionObj.isValidZPosition()){
 			positionObj.fall(dt,positionObj.getCubeBelow());
@@ -72,17 +105,36 @@ public abstract class Object {
 		}
 	}
 	
+	/**
+	 * Variable reflecting whether or not this object is terminated.
+	 */
 	private boolean isTerminated;
 	
+	
+	/**
+	 * Check whether this object is terminated.
+	 */
 	protected boolean isTerminated() {
 		return this.isTerminated;
 	}
 	
+	/**
+	 * Terminate this object.
+	 * 
+	 *@post This object is terminated.
+	 */
 	protected void terminate(){
 		this.isTerminated = true;
 	}
 	
+	/**
+	 * Bring the object back into the game world.
+	 * 
+	 * @post This object is back into the game world.
+	 */
 	protected void revive() {
 		this.isTerminated = false;
 	}
+	
+
 }
