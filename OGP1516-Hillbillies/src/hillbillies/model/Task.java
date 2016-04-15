65d98b54c1d4ca608608678fc714c21bbf318dca
0maxime <pittomvils-maxime@hotmail.com>
2016-04-15 14:23:18 +0200

    task wthhh

--- OGP1516-Hillbillies/src/hillbillies/model/Task.java
+++ OGP1516-Hillbillies/src/hillbillies/model/Task.java
@@ -2,4 +2,64 @@ package hillbillies.model;
 
 public class Task {
 
+	protected Task(String name, int priority, Statement activity, int[] selectedCube ){
+		setName(name);
+		setPriority(priority);
+		setActivity(activity);
+		setSelectedCube(selectedCube);
+	}
+	
+	// NAME  
+	public String getName(){
+		return this.name;
+	}
+	
+	private void setName(String name){
+		this.name = name;
+	}
+	
+	private String name;
+
+	
+	// PRIORITY
+	public int getPriority(){
+		return this.priority;
+	}
+
+	protected void setPriority(int priority){
+		this.priority = priority;
+	}
+	
+	private int priority;
+
+	
+	// ACTIVITY
+	
+	public Statement getActivity(){
+		return this.activity;
+	}
+	
+	private void setActivity(Statement activity){
+		this.activity = activity;
+	}
+	
+	private Statement activity;
+
+	// SELECTED CUBE
+	
+	public int[] getSelectedCube(){
+		return this.selectedCube;
+	}
+	
+	private void setSelectedCube(int[] selectedCube){
+		this.selectedCube = selectedCube;
+	}
+	
+	private int[] selectedCube;	
+	
+
 }
+
+
+
+
