de03453eb36d01f291ee905f681e2b8a636f8f4d
Win <VermaelenJan@users.noreply.github.com>
2016-05-07 18:04:39 +0200

    her-her-aanpassing

--- OGP1516-Hillbillies/src/hillbillies/model/expression/ReadVariable.java
+++ OGP1516-Hillbillies/src/hillbillies/model/expression/ReadVariable.java
@@ -35,6 +35,6 @@ public class ReadVariable extends Expression{
 
 	@Override
 	public Boolean isWellFormed(Task task, ArrayList<Object> calledBy) {
-		return true;
+		return task.getVariables().containsKey(variableName); //TODO: ge had misschien gelijk hier :p ma test het dubbel als ge wilt
 	}
 }
